d41b7c5842c6e6b9a9cc1ae9680ccfa900bb7949
IvanGML <somovlife@gmail.com>
2017-07-10 13:27:09 +0300

    One more commit Redy check for work at home

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,26 +8,36 @@ class Form extends React.Component {
         super(props);
 
         this.state = {
-
+            title: '',
         }
         
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
     
     handleSubmit(e) {
         e.preventDefault();
-        if (this.refs.title.value !== '') {
-            this.props.onAdd(this.refs.title.value);
-            this.refs.title.value = '';
+        if (this.state.title !== '') {
+            this.props.onAdd(this.state.title);
+            this.setState({ title: ''});
         } else {
             alert('Fill input area, please.')
         }
     }
 
+    handleClick(e) {
+        let title = e.target.value;
+        this.setState({ title })
+    }
+
     render(){
         return(
             <form className="todo-form" onSubmit={this.handleSubmit}>
-                <input type="text" ref="title" placeholder="Что нужно сделать"/>
+                <input
+                    type="text" 
+                    value={this.state.title} 
+                    onChange={this.handleClick} 
+                    placeholder="Что нужно сделать?"/>
                 <DeleteButton type="submit">Отправить</DeleteButton> 
             </form> 
         );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,12 +3,48 @@ import React from 'react';
 import Checkbox from './Checkbox'
 import DeleteButton from './DeleteButton'
 
-const Todo = props =>
-        <div className={`todo${props.completed ? ' completed' : ''}`}>
-            <Checkbox completed={props.completed} statusChange={() => props.statusChange(props.id)}/>
-            <span className="todo-title">{props.todo}</span>
-            <DeleteButton icon="delete icon" className="delete icon" deleteTodo={()=>props.deleteTodo(props.id)} />
-        </div>;
+
+class Todo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            editing: false,
+        }
+        // this.edit = this.edit.bind(this);
+        // this.save = this.save.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    ;(e) {
+        e.preventDefault();
+        let title = this.refs.title.value;
+        this.props.onEdit(this.props.id, title);
+        this.setState({editing: false})
+    }
+    // edit() {
+    //     this.setState({editing: false});
+    // }
+    // save() {
+    //     this.setState({editing: true});
+    // }
+    render(){
+        return(
+            this.state.editing 
+            ?
+            <form onSubmit={this.handleSubmit} className="todo-edit-form">
+                  <input type="text" ref="title" defaultValue={this.props.todo}/>
+                  <DeleteButton className="save icon" icon="save" type="submit" 
+                  onClick={this.edit}/> 
+            </form>
+            :
+            <div className={`todo${this.props.completed ? ' completed' : ''}`}>
+                <Checkbox completed={this.props.completed} statusChange={() => this.props.statusChange(this.props.id)}/>
+                <span className="todo-title">{this.props.todo}</span>
+                <DeleteButton className="edit icon" icon="edit" onClick={this.save} />
+                <DeleteButton icon="delete icon" className="delete icon" deleteTodo={()=>this.props.deleteTodo(this.props.id)} />
+            </div>
+        );
+    }
+}
 
 Todo.propTypes = {
     todo: React.PropTypes.string.isRequired,
